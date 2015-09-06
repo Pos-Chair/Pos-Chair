7758d3ab72ad13ecb433e12d59e45976294fc3ea
gopinathdanda <gopinath.danda@gmail.com>
2015-09-06 03:32:46 -0400

    Add circuit diagram.

--- spark/sensors.ino
+++ spark/sensors.ino
@@ -1,20 +1,17 @@
 int backL = A0;
 int backR = A1;
 int seatL = A2;
-int seatM = A3;
-int seatR = A4;
-double bl,br,sl,sm,sr;
+int seatR = A3;
+double bl,br,sl,sr;
 
 void setup() {
     pinMode(backL, INPUT);
 	pinMode(backR, INPUT);
 	pinMode(seatL, INPUT);
-	pinMode(seatM, INPUT);
 	pinMode(seatR, INPUT);
 	Spark.variable("backL", &bl, DOUBLE);
 	Spark.variable("backR", &br, DOUBLE);
 	Spark.variable("seatL", &sl, DOUBLE);
-	Spark.variable("seatM", &sm, DOUBLE);
 	Spark.variable("seatR", &sr, DOUBLE);
 }
 
@@ -22,6 +19,5 @@ void loop() {
 	bl = analogRead(backL);
 	br = analogRead(backR);
 	sl = analogRead(seatL);
-	sm = analogRead(seatM);
 	sr = analogRead(seatR);
 }
